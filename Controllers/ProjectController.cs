f02a9742de3c07ce1e058795aa85b4e552438f56
Talalovic II <talalovic@Talalovics-MacBook-Pro.local>
2025-01-21 12:19:14 -0500

    Initial commit after reinitializing the repository

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+using COMP2139_ICE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_ICE.Controllers;
+
+public class ProjectController : Controller
+{
+
+    
+    /// <summary>
+    /// index action will retrieve a listing of projects (database)
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var projects = new List<Project>
+        {
+            new Project { ProjectId = 1, Name = "Project 1", Description = "Last Project " },
+        };
+        return View(projects);
+    }
+    
+    
+    
+    
+    
+}
